c9a9d689adbc600a424449fe129d4d700ab39da9
irinapetrenko.1177@gmail.com <Republic150!>
2019-07-07 18:43:00 +0300

    java

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+ import java.util.Scanner;
+
+ public class Main {
+
+    public static void main(String[] args) {
+        int a=1;
+        int b=48;
+        while (true) {
+            System.out.print("input :");
+            Scanner sc = new Scanner(System.in);
+            int value = sc.nextInt();
+            b += a + value + (4);
+            a = b - value*2 ;
+            if (b > a) {
+                System.out.println("good");
+            } else {
+                System.out.println("bad");
+            }
+        }
+    }
+}
